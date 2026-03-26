67af6a112dad41be12843ad2bf8a20c63fd251cd
Heng Sovannara <sovannaraheng@gmail.com>
2026-03-26 22:01:06 +0700

    second commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Someaknea Live Stream POS Guideline',
+  tagline: 'Purchasing is under control',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -75,7 +75,7 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Someaknea-Tech',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
